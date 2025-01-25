9ca3c1743f4bdbaecf4554327bf83f8d96db76f9
Joel Polso <joel.polso@analogway.com>
2025-01-25 17:29:10 +0200

    Alternative bubble shader

--- FGJ2025/Assets/Code/BubbleDissolve.cs
+++ FGJ2025/Assets/Code/BubbleDissolve.cs
@@ -4,14 +4,11 @@ public class BubbleDissolve : MonoBehaviour
 {
 	ParticleSystem ps;
 	MeshRenderer mr;
-	float fresnel;
 
 	void Awake()
 	{
 		ps = GetComponent<ParticleSystem>();
 		mr = GetComponent<MeshRenderer>();
-
-		fresnel = mr.materials[0].GetFloat("_FresnelStrength");
 	}
 
 	void Update()
@@ -19,7 +16,6 @@ public class BubbleDissolve : MonoBehaviour
 		float pos = ps.time / ps.main.duration;
 		MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
 		propertyBlock.SetFloat("_Dissolve", pos);
-		propertyBlock.SetFloat("_FresnelStrength", Mathf.Lerp(fresnel, 0.0f, pos));
 		mr.SetPropertyBlock(propertyBlock);
 	}
 }
